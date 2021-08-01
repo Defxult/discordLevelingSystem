466ba9a70465119489f28d43f9bb6ff9e33fb443
Defxult <52070034+Defxult@users.noreply.github.com>
2021-08-01 03:19:10 -0400

    Add library description

--- discordLevelingSystem/__init__.py
+++ discordLevelingSystem/__init__.py
@@ -1,3 +1,14 @@
+"""
+Discord Leveling System
+~~~~~~~~~~~~~~~~~~~~~~~
+
+A library to implement a leveling system into a discord bot. Contains features such as XP, level, ranks, and role awards.
+
+:copyright: (c) 2021 Defxult#8269
+:license: MIT
+
+"""
+
 from .announcement import LevelUpAnnouncement
 from .leveling_system import DiscordLevelingSystem
 from .role_awards import RoleAward
