84fbe246142cd58f797ab1a4b4b9d57864f022ec
Defxult <52070034+Defxult@users.noreply.github.com>
2021-08-24 18:57:56 -0400

    Future proof copyright year

--- discordLevelingSystem/__init__.py
+++ discordLevelingSystem/__init__.py
@@ -4,7 +4,7 @@ Discord Leveling System
 
 A library to implement a leveling system into a discord bot. Contains features such as XP, level, ranks, and role awards.
 
-:copyright: (c) 2021 Defxult#8269
+:copyright: (c) 2021-present Defxult#8269
 :license: MIT
 
 """
