b41872a13aed5888e525b6935859077ca429b3cf
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2021-09-10 13:51:07 +0530

    Create basic.py

---
+++ examples/basic.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+from discordLevelingSystem import DiscordLevelingSystem, RoleAward, LevelUpAnnouncement
+
+bot = commands.Bot(...)
+
+main_guild_id = 850809412011950121
+
+my_awards = {
+    main_guild_id : [
+        RoleAward(role_id=831672678586777601, level_requirement=1, role_name='Rookie'),
+        RoleAward(role_id=831672730583171073, level_requirement=2, role_name='Associate'),
+        RoleAward(role_id=831672814419050526, level_requirement=3, role_name='Legend')
+    ]
+}
+
+announcement = LevelUpAnnouncement(f'{LevelUpAnnouncement.Member.mention} just leveled up to level {LevelUpAnnouncement.LEVEL} 😎')
+
+# DiscordLevelingSystem.create_database_file(r'C:\Users\Defxult\Documents') database file already created
+lvl = DiscordLevelingSystem(rate=1, per=60.0, awards=my_awards, level_up_announcement=announcement)
+lvl.connect_to_database_file(r'C:\Users\Defxult\Documents\DiscordLevelingSystem.db')
+
+@bot.event
+async def on_message(message):
+    await lvl.award_xp(amount=15, message=message)
+
+bot.run(...)
