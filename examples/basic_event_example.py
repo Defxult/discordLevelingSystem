d9ea611cc527c8082000b76a228b770dd217421e
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2021-09-10 14:28:06 +0530

    Add `bot.run()`

--- examples/basic_event_example.py
+++ examples/basic_event_example.py
@@ -2,6 +2,8 @@ import discord
 from discordLevelingSystem import DiscordLevelingSystem, MemberData
 from discord.ext import commands 
 
+TOKEN = "" #your bot's token here
+
 bot = commands.Bot(command_prefix="!")
 lvl = DiscordLevelingSystem(rate=1, per=60.0, bot=bot) # your bot instance variable is needed
 lvl.connect_to_database_file(r'C:\Users\Infinix\Documents\DiscordLevelingSystem.db')
@@ -9,3 +11,5 @@ lvl.connect_to_database_file(r'C:\Users\Infinix\Documents\DiscordLevelingSystem.
 @bot.event
 async def on_dls_level_up(member: discord.Member, message: discord.Message, data: MemberData):
   await message.channel.send(f"Congratulations {member.mention}! You have levelled up!")
+
+bot.run(TOKEN)
